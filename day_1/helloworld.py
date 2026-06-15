797e28d51f451dc96ba8bf33a69c3bab39408ad5
Teaquilla <zidanezulf@student.ub.ac.id>
2026-06-16 04:20:31 +0700

    feat: complete day 1 python data types exercises

---
+++ day_1/helloworld.py
@@ -0,0 +1,16 @@
+print(2 + 3) # addition/penjumlahan(+)
+print(3 - 2) # subtraction/pengurangan(-)
+print(2 * 3) # multiplication/perkalian(*)
+print(3 / 2) # division/pembagian(/)
+print(3 % 2) # modulo/persamaan sisa bagi(%)
+print(3 ** 2) # exponentiation/pangkat(**)
+print(3 // 2) # floor division/pembagian bulat(//)
+
+#Cek tipe data
+print(type(2)) # int/integer
+print(type(2.5)) # float
+print(type("2")) # str/string
+print(type(True)) # bool/boolian
+print(type(1 + 3j)) # complex/kompleks
+print(type([1,2,3])) # list
+print(type({'nama': 'John'})) # dict/dictionary
